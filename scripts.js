1a05fc0f012717ee5659c1da1567c3f50c6d7108
Carlos Vinícius <carlosviniciusazevedo123@hotmail.com>
2026-05-27 09:45:06 -0300

    feat: adicionar suporte para conversão de Libra e atualizar a seleção de moedas

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const convertButton = document.getElementById("botao-converter");
 
 const currencySelect = document.getElementById("segundoseletor");
 
+const firstCurrencySelect=document.getElementById("primeiroseletor");
+
 function convertvalues() {
 
     const inputConvertValue = document.getElementById("valor").value
@@ -13,6 +15,14 @@ function convertvalues() {
     const dolarToday = 5.04
     const euroToday = 5.87
     const bitcoinToday = 383060.54
+    const libraToday = 6.76
+
+    if (currencySelect.value == "libra") {
+        segundoValo.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputConvertValue / libraToday)
+    }
 
     if (currencySelect.value == "dolar") {
         segundoValo.innerHTML = new Intl.NumberFormat("en-US", {
@@ -42,24 +52,66 @@ function convertvalues() {
 
 }
 function changeCurrency() {
+    
+    const firstCurrencyChange = document.getElementById("primeiramoeda")
+    const firstCurrencyimage = document.getElementById("primeiraimg")
     const currencyChange = document.getElementById("segundamoeda")
     const currencyimage = document.getElementById("segundaimg")
 
     
+    if (firstCurrencySelect.value == "dolar") {
+        firstCurrencyChange.innerHTML = "Dólar Americano"
+        firstCurrencyimage.src = "./img/dolar.png"
+    }
+
     if (currencySelect.value == "dolar") {
         currencyChange.innerHTML = "Dólar Americano"
         currencyimage.src = "./img/dolar.png"
     }
+    
+    if (firstCurrencySelect.value == "euro") {
+        firstCurrencyChange.innerHTML = "Euro"
+        firstCurrencyimage.src = "./img/euro.png"
+    }
+    
     if (currencySelect.value == "euro") {
         currencyChange.innerHTML = "Euro"
         currencyimage.src = "./img/euro.png"
     }
+
+    if (firstCurrencySelect.value == "bitcoin") {
+        firstCurrencyChange.innerHTML = "BTC"
+        firstCurrencyimage.src = "./img/bitcoin.png"
+    }
+
     if (currencySelect.value == "bitcoin") {
         currencyChange.innerHTML = "BTC"
         currencyimage.src = "./img/bitcoin.png"
     }
+     
+    if (firstCurrencySelect.value == "real") {
+        firstCurrencyChange.innerHTML = "BRL"
+        firstCurrencyimage.src = "./img/brasil.png"
+    }
+    
+    if (currencySelect.value == "real") {
+        currencyChange.innerHTML = "BRL"
+        currencyimage.src = "./img/brasil.png"
+    }
+    
+    if (firstCurrencySelect.value == "libra") {
+        firstCurrencyChange.innerHTML = "GBN"
+        firstCurrencyimage.src = "./img/UK.png"
+    }
+    
+    if (currencySelect.value == "libra") {
+        currencyChange.innerHTML = "GBN"
+        currencyimage.src = "./img/UK.png"
+    }
 }
 
 currencySelect.addEventListener("change", changeCurrency)
+
+firstCurrencySelect.addEventListener("change", changeCurrency)
 
 convertButton.addEventListener("click", convertvalues)
